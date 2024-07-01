32e18e4bebf4078f2ecdf5f880fed233427af50f
cwb1 <1090378278@qq.com>
2024-07-01 21:10:48 +0800

    add hal_tick

--- rtos/board.cpp
+++ rtos/board.cpp
@@ -11,7 +11,7 @@ D2_MEM_ALIGN(M_ALIGN_SIZE) static uint8_t memHeap[MEM_HEAP_SIZE];
 extern "C" void SysTick_Handler(void)
 {
     mIrq::getInstance()->interruptEnter();
-
+    HAL_IncTick();
     mClock::getInstance()->tickIncrease();
     mIrq::getInstance()->interruptLeave();
 }
