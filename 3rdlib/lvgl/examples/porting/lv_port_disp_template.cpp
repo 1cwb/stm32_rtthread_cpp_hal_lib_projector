b498da3b9660a0a3889a54f3af719800761a3522
tony <1090378278@qq.com>
2025-04-12 14:30:08 +0800

    xx

--- 3rdlib/lvgl/examples/porting/lv_port_disp_template.cpp
+++ 3rdlib/lvgl/examples/porting/lv_port_disp_template.cpp
@@ -87,14 +87,14 @@ void lv_port_disp_init(void)
     static lv_color_t buf_1[MY_DISP_HOR_RES * 10];                          /*A buffer for 10 rows*/
     lv_disp_draw_buf_init(&draw_buf_dsc, buf_1, NULL, MY_DISP_HOR_RES * 10);   /*Initialize the display buffer*/
 #endif
-#if (!USE_SDRAM)
+
     /* Example for 2) */
     //static lv_color_t buf_2_1[MY_DISP_HOR_RES * 10];// = new lv_color_t[MY_DISP_HOR_RES * 10];                        /*A buffer for 10 rows*/
     //static lv_color_t buf_2_2[MY_DISP_HOR_RES * 10];// = new lv_color_t[MY_DISP_HOR_RES * 10];                        /*An other buffer for 10 rows*/
-    static lv_color_t* buf_2_1 = (lv_color_t*)(new alignas(32) uint8_t [MY_DISP_HOR_RES * 10* sizeof(lv_color_t)]);//lv_color_t[MY_DISP_HOR_RES * 10];
-    static lv_color_t* buf_2_2 = (lv_color_t*)(new alignas(32) uint8_t [MY_DISP_HOR_RES * 10* sizeof(lv_color_t)]); //lv_color_t[MY_DISP_HOR_RES * 10];
-    lv_disp_draw_buf_init(&draw_buf_dsc, buf_2_1, buf_2_2, MY_DISP_HOR_RES * 10);   /*Initialize the display buffer*/
-#else 
+    static lv_color_t* buf_2_1 = (lv_color_t*)(new alignas(32) uint8_t [MY_DISP_HOR_RES * 20* sizeof(lv_color_t)]);//lv_color_t[MY_DISP_HOR_RES * 10];
+    static lv_color_t* buf_2_2 = (lv_color_t*)(new alignas(32) uint8_t [MY_DISP_HOR_RES * 20* sizeof(lv_color_t)]); //lv_color_t[MY_DISP_HOR_RES * 10];
+    lv_disp_draw_buf_init(&draw_buf_dsc, buf_2_1, buf_2_2, MY_DISP_HOR_RES * 20);   /*Initialize the display buffer*/
+#if 0
     /* Example for 3) also set disp_drv.full_refresh = 1 below*/
     //static lv_color_t buf_3_1[MY_DISP_HOR_RES * MY_DISP_VER_RES];            /*A screen sized buffer*/
     //static lv_color_t buf_3_2[MY_DISP_HOR_RES * MY_DISP_VER_RES];            /*Another screen sized buffer*/
