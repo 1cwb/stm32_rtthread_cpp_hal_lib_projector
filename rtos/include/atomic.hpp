540428cfd231627d6f7d018a1daf77eeb060f18d
tony <1090378278@qq.com>
2025-08-06 12:49:25 +0800

    add atomic

--- rtos/include/atomic.hpp
+++ rtos/include/atomic.hpp
@@ -1,228 +1,166 @@
 #pragma once
 #include "mhw.hpp"
+#include <type_traits>
+
+// 内存序枚举（简化版）
+enum memory_order {
+    relaxed = __ATOMIC_RELAXED,
+    consume = __ATOMIC_CONSUME,
+    acquire = __ATOMIC_ACQUIRE,
+    release = __ATOMIC_RELEASE,
+    acq_rel = __ATOMIC_ACQ_REL,
+    seq_cst = __ATOMIC_SEQ_CST
+};
+
 template <typename T>
 class mAtomic
 {
-#if defined (__GNUC__)                /* GNU GCC Compiler */
+    static_assert(std::is_integral<T>::value || std::is_pointer<T>::value,
+                 "mAtomic only supports integral and pointer types");
+    static_assert(sizeof(T) <= sizeof(void*),
+                 "mAtomic does not support types larger than pointer size");
+
 public:
-    mAtomic() = default;
-    explicit mAtomic(T value) : _value(value) {}
-    inline T load()
-    {
+    mAtomic() noexcept = default;
+    explicit mAtomic(T value) noexcept : _value(value) {}
+    
+    // 禁止拷贝和赋值
+    mAtomic(const mAtomic&) = delete;
+    mAtomic& operator=(const mAtomic&) = delete;
+
+    // 加载操作
+    template <memory_order order = seq_cst>
+    T load() const noexcept {
         T oldval;
-        do
-        {
+        do {
             oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval, &_value)) != 0U);
+        } while (__STREXW(oldval, &_value) != 0U);
         return oldval;
     }
-    /**
-	 * Atomically store a value
-	 */
-	inline void store(T value)
-	{
-        do
-        {
+
+    // 存储操作
+    template <memory_order order = seq_cst>
+    void store(T value) noexcept {
+        do {
             __LDREXW(&_value);
-        } while ((__STREXW(value, &_value)) != 0U);
-	}
-	/**
-	 * Atomically add a number and return the previous value.
-	 * @return value prior to the addition
-	 */
-	inline void fetchAdd(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval + value, &_value)) != 0U);
+        } while (__STREXW(value, &_value) != 0U);
     }
-	/**
-	 * Atomically substract a number and return the previous value.
-	 * @return value prior to the substraction
-	 */
-	inline void fetchSub(T value)
-    {
+
+    // 交换操作
+    template <memory_order order = seq_cst>
+    T exchange(T value) noexcept {
         T oldval;
-        do
-        {
+        do {
             oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval - value, &_value)) != 0U);
-    }
-	/**
-	 * Atomic AND with a number
-	 * @return value prior to the operation
-	 */
-	inline void fetchAnd(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval & value, &_value)) != 0U);
-    }
-	/**
-	 * Atomic XOR with a number
-	 * @return value prior to the operation
-	 */
-	inline void fetchXor(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval ^ value, &_value)) != 0U);
-    }
-	/**
-	 * Atomic OR with a number
-	 * @return value prior to the operation
-	 */
-	inline void fetchOr(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval | value, &_value)) != 0U);
-    }
-	/**
-	 * Atomic compare and exchange operation.
-	 * This compares the contents of _value with the contents of *expected. If
-	 * equal, the operation is a read-modify-write operation that writes desired
-	 * into _value. If they are not equal, the operation is a read and the current
-	 * contents of _value are written into *expected.
-	 * @return If desired is written into _value then true is returned
-	 */
-	inline T compareExchange(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(value, &_value)) != 0U);
+        } while (__STREXW(value, &_value) != 0U);
         return oldval;
     }
-    inline void flagClear()
-    {
-        do
-        {
-            __LDREXW(&_value);
-        } while ((__STREXW(0, &_value)) != 0U);
-    }
-    inline void flagTestAndSet()
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(1, &_value)) != 0U);
-    }
-    inline bool exchangeStrong(T *old, T newval)
-    {
-        T result;
-        T temp = *old;
-        do
-        {
+
+    // 比较交换操作
+    template <memory_order order = seq_cst>
+    bool compare_exchange_strong(T& expected, T desired) noexcept {
+        T result = expected;
+        do {
             result = __LDREXW(&_value);
-            if (result != temp)
-            {
-                *old = result;
-                __STREXW(result, &_value);
-                break;
+            if (result != expected) {
+                expected = result;
+                __STREXW(result, &_value); // 释放锁
+                return false;
             }
-        } while ((__STREXW(newval, &_value)) != 0U);
-        return (result == temp);
+        } while (__STREXW(desired, &_value) != 0U);
+        return true;
     }
-    inline void operator++()
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval + 1, &_value)) != 0U);
-    }
-    inline void operator++(int)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval + 1, &_value)) != 0U);
-    }
-    inline void operator--()
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval - 1, &_value)) != 0U);
-    }
-    inline void operator--(int)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval - 1, &_value)) != 0U);
-    }
-    inline void operator|=(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval | value, &_value)) != 0U);
-    }
-    inline void operator&=(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval & value, &_value)) != 0U);
-    }
-    inline void operator^=(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval ^ value, &_value)) != 0U);
-    }
-    inline void operator+=(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval + value, &_value)) != 0U);
-    }
-    inline void operator-=(T value)
-    {
-        T oldval;
-        do
-        {
-            oldval = __LDREXW(&_value);
-        } while ((__STREXW(oldval - value, &_value)) != 0U);
-    }
-private:
-    __attribute__((always_inline))     static inline T __LDREXW(volatile T *addr)
-    {
-        T result;
 
-        __asm volatile ("ldrex %0, %1" : "=r" (result) : "Q" (*addr) );
+    // 原子加法并返回之前的值
+    template <memory_order order = seq_cst>
+    T fetch_add(T value) noexcept {
+        T oldval;
+        do {
+            oldval = __LDREXW(&_value);
+        } while (__STREXW(oldval + value, &_value) != 0U);
+        return oldval;
+    }
+
+    // 原子减法并返回之前的值
+    template <memory_order order = seq_cst>
+    T fetch_sub(T value) noexcept {
+        T oldval;
+        do {
+            oldval = __LDREXW(&_value);
+        } while (__STREXW(oldval - value, &_value) != 0U);
+        return oldval;
+    }
+
+    // 原子与操作并返回之前的值
+    template <memory_order order = seq_cst>
+    T fetch_and(T value) noexcept {
+        T oldval;
+        do {
+            oldval = __LDREXW(&_value);
+        } while (__STREXW(oldval & value, &_value) != 0U);
+        return oldval;
+    }
+
+    // 原子或操作并返回之前的值
+    template <memory_order order = seq_cst>
+    T fetch_or(T value) noexcept {
+        T oldval;
+        do {
+            oldval = __LDREXW(&_value);
+        } while (__STREXW(oldval | value, &_value) != 0U);
+        return oldval;
+    }
+
+    // 原子异或操作并返回之前的值
+    template <memory_order order = seq_cst>
+    T fetch_xor(T value) noexcept {
+        T oldval;
+        do {
+            oldval = __LDREXW(&_value);
+        } while (__STREXW(oldval ^ value, &_value) != 0U);
+        return oldval;
+    }
+
+    // 标志操作
+    void clear_flag() noexcept {
+        store(0);
+    }
+
+    bool test_and_set_flag() noexcept {
+        return exchange(1) != 0;
+    }
+
+    // 运算符重载
+    T operator=(T value) noexcept { store(value); return value; }
+    operator T() const noexcept { return load(); }
+
+    mAtomic& operator++() noexcept { fetch_add(1); return *this; }
+    T operator++(int) noexcept { return fetch_add(1); }
+    mAtomic& operator--() noexcept { fetch_sub(1); return *this; }
+    T operator--(int) noexcept { return fetch_sub(1); }
+
+    mAtomic& operator+=(T value) noexcept { fetch_add(value); return *this; }
+    mAtomic& operator-=(T value) noexcept { fetch_sub(value); return *this; }
+    mAtomic& operator&=(T value) noexcept { fetch_and(value); return *this; }
+    mAtomic& operator|=(T value) noexcept { fetch_or(value); return *this; }
+    mAtomic& operator^=(T value) noexcept { fetch_xor(value); return *this; }
+
+private:
+    // ARM独占访问指令封装
+    __attribute__((always_inline)) 
+    static inline T __LDREXW(volatile T* addr) {
+        T result;
+        __asm volatile ("ldrex %0, %1" : "=r" (result) : "Q" (*addr));
         return result;
     }
-    __attribute__((always_inline))     static inline T __STREXW(volatile T value, volatile T *addr)
-    {
-        T result;
 
-        __asm volatile ("strex %0, %2, %1" : "=&r" (result), "=Q" (*addr) : "r" (value) );
+    __attribute__((always_inline)) 
+    static inline T __STREXW(T value, volatile T* addr) {
+        T result;
+        __asm volatile ("strex %0, %2, %1" : "=&r" (result), "=Q" (*addr) : "r" (value));
         return result;
     }
-private:
-    volatile T _value;
 
-#endif
+private:
+    alignas(sizeof(T)) volatile T _value;
 };
